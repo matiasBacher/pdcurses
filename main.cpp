5a519c715d9d2e9f3f1fc87bdfcc3ff3704be79e
matiasdrianbacher.2006@gmail.com <matiasdrianbacher.2006@gmail.com>
2020-09-16 17:50:50 -0300

    agregamos colores

--- main.cpp
+++ main.cpp
@@ -4,7 +4,13 @@ int main () {
  char cadena[128];
  initscr();
  move (10,2); 
+ if (has_colors()) {
+ start_color();
+ init_pair(1,COLOR_RED,COLOR_YELLOW);
+ attron(COLOR_PAIR(1));
  printw("Escribe la contraseña: ");
+ attroff(COLOR_PAIR(1));
+ } 
  noecho();
  scanw("%s",cadena);
  echo(); 
